b8e01fcbcd3d85b5bc3b3b7815007626d66871e7
Manjit Singh <manjitsingh23@gmail.com>
2014-03-04 14:09:29 +0000

    refactored by defining variable bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,26 +1,21 @@
-# link to Bike class
+
 require_relative "../lib/bike"
 
-# we're describing the functionality of a specific class called Bike
+
 describe Bike do
 
-	# this is a specific feature(behaviour) that we expect to be present
-	it "should not be broken after we create it" do
-		bike = Bike.new # initialize a new object of Bike class
+  let(:bike) {Bike.new}  # calls rspec helper "let" 
 
-		#expect an instance of this class 
-		#to have a method "broken?" that should return false
-		expect(bike).not_to be_broken
+	it "should not be broken after we create it" do 
+    expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
 
   it "should be able to get fixed" do
-    bike = Bike.new
     bike.break
     bike.fix
     expect(bike).not_to be_broken
