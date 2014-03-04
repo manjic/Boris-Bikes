0c7e86ce348b7dbe6b082d8e07462ac31b851c71
Manjit Singh <manjitsingh23@gmail.com>
2014-03-04 15:10:04 +0000

    Docking station releases the bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,19 +3,21 @@ require_relative "../lib/bike"
 
 
 describe DockingStation do
+  let (:bike) {Bike.new}
+  let (:station) {DockingStation.new}
 	
 	it "should accept a bike" do
 		bike = Bike.new
 		station = DockingStation.new
-
-		#we expect the station to have 0 bikes
 		expect(station.bike_count).to eq(0)
-		
-    #lets dock a bike into the station
 		station.dock(bike)
-		
-    #now we expect the station to have 1 bike
 		expect(station.bike_count).to eq(1)
-	end
+  end
+
+  it "should release a bike" do
+    station.dock(bike)
+    station.release(bike)
+    expect(station.bike_count).to eq(0)
+  end
 
 end
